18ff6331397f5412e0a8c5f556f38ac6e23a7d92
Ashish Tiwari <ashishtwr314@gmail.com>
2025-03-01 16:47:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,58 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./main.css";
 
+function shortFunction() {
+    console.log("This is a short function.");
+    return 42;
+}
+
+function longFunction() {
+    console.log("This function is too long.");
+    let sum = 0;
+    for (let i = 0; i < 10; i++) {
+        sum += i;
+        console.log(sum);
+    }
+    console.log("Still running...");
+    console.log("This function should be refactored.");
+    console.log("Consider breaking it into smaller functions.");
+    console.log("It exceeds the recommended 10 lines.");
+    console.log("Refactor this!");
+    console.log("End of long function.");
+    return sum;
+}
+
+function deeplyNestedFunction() {
+    if (true) {
+        if (true) {
+            if (true) {
+                if (true) { // Nesting Level 4
+                    console.log("This function is deeply nested.");
+                }
+            }
+        }
+    }
+}
+
+const arrowFunction = () => {
+    console.log("This is an arrow function.");
+};
+
+const longAndNested = function () {
+    console.log("Starting function...");
+    let count = 0;
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (i === j) {
+                console.log("Nested too much!"); // Nesting Level 3
+            }
+        }
+    }
+    console.log("Function complete.");
+};
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
